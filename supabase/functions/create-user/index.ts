07a0b44bb55e5cb74693ffdfc3667a25c48e5622
Daksh Verma <asofbd.attendance@gmail.com>
2026-03-14 13:10:15 +0530

    Update index.ts

--- supabase/functions/create-user/index.ts
+++ supabase/functions/create-user/index.ts
@@ -1,6 +1,6 @@
 // supabase/functions/create-user/index.ts
 // Deploy with: supabase functions deploy create-user
-
+//This is a tst thing
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
